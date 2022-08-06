27c80572c8aedd2a4a33be1d95def59d71aadede
Brian <bwmcname@gmail.com>
2022-08-06 19:07:12 -0400

    rework allocation tracking

--- allocator_interface.h
+++ allocator_interface.h
@@ -6,6 +6,7 @@
 
 
 #if 0
+
 void *OnAlloc(void *newMem, size_t size, uint32_t alignment, int line, const char *file)
 {
     // Do something to track the allocation
@@ -24,23 +25,67 @@ void OnFree(void *freed, int line, const char *file)
 }
 #endif
 
-#if defined(TRACKALLOC) || defined(TRACKREALLOC) || defined(TRACKFREE)
-#define DECLARE_ALLOCATOR_INTERFACE_METHODS()                                         \
+#ifdef MEM_TRACKING_ENABLED
+#ifndef PREALLOC_CALLBACK
+#define PREALLOC_CALLBACK(size, alignment, line, file) PreAllocThunk(size, alignment, line, file)
+void PreAllocThunk(size_t size, uint32_t alignment, int line, const char *file) {}
+#endif
+#ifndef POSTALLOC_CALLBACK
+#define POSTALLOC_CALLBACK(newMem, size, alignment, line, file) PostAllocThunk(newMem, size, alignment, line, file);
+void PostAllocThunk(void *newMem, size_t size, uinit32_t alignment, int line, const char *file) { }
+#endif
+#ifndef PREREALLOC_CALLBACK
+#define PREREALLOC_CALLBACK PreReAllocThunk(oldAlloc, size, line, file)
+void PreReAllocThunk(void *oldAlloc, size_t size, int line, const char *file) {}
+#endif
+#ifndef POSTREALLOC_CALLBACK
+#define POSTREALLOC_CALLBACK(newMem, oldMem, size, line, file) PostReAllocThunk(newMem, oldMem, size, line, file)
+void PostReAllocThunk(void *newMem, void *oldMem, size_t size, int line, const char *file) { }
+#endif
+#ifndef PREFREE_CALLBACK
+#define PREFREE_CALLBACK(mem, line, file) PreFreeThunk(mem, line, file)
+void PreFreeThunk(void *mem, int line, const char *file) {}
+#endif
+#ifndef POSTFREE_CALLBACK
+#define POSTFREE_CALLBACK(mem, line, file) PostFreeThunk(mem, line, file)
+void PostFreeThunk(void *mem, int line, const char *file) {}
+#endif
+#endif
+
+#ifdef MEM_TRACKING_ENABLED
+#define DECLARE_ALLOCATOR_INTERFACE_METHODS()                           \
     __pragma(warning(push, 0))                                          \
     void *AllocInternal(size_t size, uint32_t alignment, int line, const char *file); \
-    void FreeInternal(void *addr, int line, const char *file);                        \
-    void *ReAllocInternal(void *addr, size_t size, int line, const char *file);       \
-    inline void *TrackedAllocInternal(size_t size, uint32_t alignment, int line, const char *file) { return TRACKALLOC(AllocInternal(size, alignment, line, file), size, alignment, line, file); } \
-    inline void TrackedFreeInternal(void *addr, int line, const char *file) { FreeInternal(addr, line, file); TRACKFREE(addr, line, file); } \
-    inline void *TrackedReAllocInternal(void *addr, size_t size, int line, const char *file) { return TRACKREALLOC(ReAllocInternal(addr, size, line, file), addr, size, line, file); } \
-    __pragma(warning(pop))
+    void FreeInternal(void *addr, int line, const char *file);          \
+    void *ReAllocInternal(void *addr, size_t size, int line, const char *file); \
+    inline void *TrackedAllocInternal(size_t size, uint32_t alignment, int line, const char *file) \
+    {                                                                   \
+        PREALLOC_CALLBACK(size, alignment, line, file);                 \
+        void *result = AllocInternal(size, alignment, line, file);  \
+        POSTALLOC_CALLBACK(result, size, alignment, line, file);        \
+        return result;                                                  \
+    }                                                                   \
+    inline void TrackedFreeInternal(void *addr, int line, const char *file) \
+    {                                                                   \
+        PREFREE_CALLBACK(addr, line, file);                             \
+        FreeInternal(addr, line, file);                                 \
+        POSTFREE_CALLBACK(addr, line, file);                            \
+    }                                                                   \
+    inline void *TrackedReAllocInternal(void *addr, size_t size, int line, const char *file) \
+    {                                                                   \
+        PREREALLOC_CALLBACK(addr, size,  line, file);                   \
+        void *result = ReAllocInternal(addr, size, line, file);         \
+        POSTREALLOC_CALLBACK(result, addr, size, line, file);           \
+        return result;                                                  \
+    }                                                                   \
+    __pragma(warning(pop)) 
 #define ALLOC(size, alignment) TrackedAllocInternal(size, alignment, __LINE__, __FILE__)
 #define FREE(addr) TrackedFreeInternal(addr, __LINE__, __FILE__)
 #define REALLOC(addr, size) TrackedReAllocInternal(addr, size, __LINE__, __FILE__)
 #else
-#define DECLARE_ALLOCATOR_INTERFACE_METHODS()                                         \
+#define DECLARE_ALLOCATOR_INTERFACE_METHODS()                           \
     void *AllocInternal(size_t size, uint32_t alignment, int line, const char *file); \
-    void FreeInternal(void *addr, int line, const char *file);                        \
+    void FreeInternal(void *addr, int line, const char *file);          \
     void *ReAllocInternal(void *addr, size_t size, int line, const char *file)
 
 #define ALLOC(size, alignment) AllocInternal(size, alignment, __LINE__, __FILE__)
