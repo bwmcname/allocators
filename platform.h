dfa9d38667b3fd52cec34d75ae2c1ab66d81d355
Brian <bwmcname@gmail.com>
2022-08-16 23:17:03 -0400

    don't warn when including windows.h

--- platform.h
+++ platform.h
@@ -2,7 +2,9 @@
 #pragma once
 
 #ifdef _WIN32
+#pragma warning(push, 0)
 #include <windows.h>
+#pragma warning(pop)
 #define ICE(dest, exc, comp) (InterlockedCompareExchange(dest, exc, comp))
 #endif
 
